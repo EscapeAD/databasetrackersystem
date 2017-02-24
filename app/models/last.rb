3c9ee2aa460a7143067b0861e7ab058f1d7c2e79
Adam Tse <mr.adam.tse@gmail.com>
2017-02-24 17:15:35 -0500

    added class for event updates

---
+++ app/models/last.rb
@@ -0,0 +1,16 @@
+class Last
+
+
+  def self.last
+    final = Person.last(20).concat(Reserve.last(20)).sort_by {|x| x['created_at']}
+                                                    .reverse.last(20)
+    return final.map do |check|
+      if Person.all.include? check
+         "#{check.created_at} - #{check.name} added to database"
+       elsif Reserve.all.include? check
+         "#{check.created_at} - #{Person.find(check.person_id).name} attended event #{Event.find(check.event_id).name}"
+      end
+    end
+  end
+
+end
