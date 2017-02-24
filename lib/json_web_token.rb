b14e1097c24ffeda0144842e5078edda17cafc82
Adam Tse <mr.adam.tse@gmail.com>
2017-02-24 18:40:45 -0500

    added jwt/figaro + jwt session

---
+++ lib/json_web_token.rb
@@ -0,0 +1,11 @@
+class JsonWebToken
+  def self.encode(payload)
+    JWT.encode(payload, Figaro.env.jwt_secret)
+  end
+
+  def self.decode(token)
+    return HashWithIndifferentAccess.new(JWT.decode(token, Figaro.env.jwt_secret)[0])
+  rescue
+    nil
+  end
+end
